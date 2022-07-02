24727b788b9c3c5a0a2d9f2b23cc1474b8f9b3e8
junghyeonsu <jung660317@naver.com>
2022-07-02 23:49:13 +0900

    MISSON COMPLELTE!!!

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,21 +4,17 @@
 
 import * as $0 from "./routes/[name].tsx";
 import * as $1 from "./routes/api/joke.ts";
-import * as $2 from "./routes/countdown.tsx";
-import * as $3 from "./routes/index.tsx";
-import * as $$0 from "./islands/Countdown.tsx";
-import * as $$1 from "./islands/Counter.tsx";
+import * as $2 from "./routes/index.tsx";
+import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
   routes: {
     "./routes/[name].tsx": $0,
     "./routes/api/joke.ts": $1,
-    "./routes/countdown.tsx": $2,
-    "./routes/index.tsx": $3,
+    "./routes/index.tsx": $2,
   },
   islands: {
-    "./islands/Countdown.tsx": $$0,
-    "./islands/Counter.tsx": $$1,
+    "./islands/Counter.tsx": $$0,
   },
   baseUrl: import.meta.url,
 };
